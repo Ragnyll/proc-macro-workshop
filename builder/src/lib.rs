6b6d1fc9ce4218473996c8392e5a5520db8a90ab
ragnyll <ragnyll@gallowzhumour.dev>
2023-06-07 20:27:44 -0700

    test 2 builds

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,40 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident, Data, DataStruct, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let input_ident = ast.ident;
+    let builder_struct_name = format!("{}Builder", input_ident.to_string());
+    let builder_ident = Ident::new(&builder_struct_name, input_ident.span());
 
-    unimplemented!()
+    // these will always be the same order and size
+    let mut field_idents = vec![];
+    let mut field_types = vec![];
+    if let Data::Struct(data_struct) = ast.data {
+        let fields = data_struct.fields;
+        if let Fields::Named(named_fields) = fields {
+            let named_field_identifiers = named_fields.named;
+            for field in named_field_identifiers {
+                field_idents.push(field.ident);
+                field_types.push(field.ty);
+            }
+        }
+    };
+
+    let expanded = quote!{
+        pub struct #builder_ident {
+            #(#field_idents: Option<#field_types>),*
+        }
+        impl #input_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#field_idents: None),*
+                }
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
